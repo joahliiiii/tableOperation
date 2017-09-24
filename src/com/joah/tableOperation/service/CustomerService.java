c8c0eafa0ea60cab5856be3a3f57989d082dbe53
joah <joahli@qq.com>
2017-09-24 21:58:09 +0800

    添加 insert delete update 方法.

--- src/com/joah/tableOperation/service/CustomerService.java
+++ src/com/joah/tableOperation/service/CustomerService.java
@@ -1,10 +1,33 @@
 package com.joah.tableOperation.service;
 
+import com.joah.tableOperation.dao.CustomerDao;
 import com.joah.tableOperation.entity.Customer;
+
+import java.sql.SQLException;
 
 public class CustomerService {
 
-    public static void insert(Customer customer){
+    /**
+     * 添加一个新用户
+     * @param customer Customer 对象
+     * @throws SQLException 返回SQL操作异常
+     */
+    public static void insert(Customer customer) throws SQLException {
+
+        if(CustomerDao.findCustomer(customer) == null){
+            CustomerDao.insertCustomer(customer);
+        }else{
+            // 在这里写个自定义异常最好了
+        }
+    }
+
+    public static void delete(Customer cus) throws SQLException {
+        CustomerDao.deleteCustomer(cus);
+    }
+
+    public static void update(Customer cus, Customer sessionCus){
+
+
 
     }
 }
